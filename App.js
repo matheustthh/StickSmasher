49ee1f92cacab038a5fd5b0dfd2c18d83e945482
matheustthh <matheus.theodoro.silva@escola.pr.gov.br>
2024-09-30 11:45:22 -0300

    project setup

--- App.js
+++ App.js
@@ -4,9 +4,25 @@ import { StyleSheet, View} from "react-native";
 import Button from './components/Button'; 
 import ImageViewer from './components/ImageViewer';
 
+import * as ImagePicker from 'expo-image-picker';
+
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
